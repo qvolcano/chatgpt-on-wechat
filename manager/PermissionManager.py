ca013dd973e99c0126b9b91e0aa7d1c9a2ba253d
qvolcano <qvolcano@126.com>
2023-03-07 12:41:43 +0800

    Update PermissionManager.py

---
+++ manager/PermissionManager.py
@@ -0,0 +1,20 @@
+groups=[]
+users={}
+
+def setup():
+  #load config
+  pass
+  
+def setUserGroup(user,group):
+  users[user]=group
+  
+def setGroupPermission(group,permission):
+  groups[group]=permission
+  
+def getUserGroup(user):
+  return users[user]
+  
+def getUserPermission(user):
+  group=getUserGroup(user)
+  if group :
+    return group['permission']
