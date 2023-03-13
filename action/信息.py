ccf6b3ff1c7a15acf48b2c8291f3d78bdd74f01f
qvolcano <qvolcano@126.com>
2023-03-13 12:21:23 +0800

    Update 信息.py

--- action/信息.py
+++ action/信息.py
@@ -1,4 +1,10 @@
 import requests
 
-def default(args):
-    return str(args['context']['User']["MemberList"])
+def default(context):
+    if len(context.get("User")['MemberList'])>0:
+    for i in context.get("User")['MemberList']:
+        return str(i)
+    else:
+        user=context.get("User")["NickName"]
+        permission=PermissionManager.getUserGroup(user)
+    return str(user)
