2b7a5090db91c35e06162c54341ae6e8a2f8a666
qvolcano <qvolcano@126.com>
2023-03-13 11:26:42 +0800

    Update 重载服务.py

--- action/重载服务.py
+++ action/重载服务.py
@@ -3,7 +3,7 @@ def default(args):
   query=args['query']
   query.split(" ")
   if ServiceManager.status(query[1]) == "none":
-    return "服务不存在"
+    return "服务"+query[1]+"不存在"
   ServiceManager.stop(query[1])
   ServiceManager.start(query[1])
   return "服务"+query[1]+"重载完毕"
