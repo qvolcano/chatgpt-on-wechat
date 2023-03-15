dbb242c6308a3ea039ac113d7e57ec7a67f2f6cd
qvolcano <qvolcano@126.com>
2023-03-15 18:41:59 +0800

    Update OpenaiService.py

--- services/OpenaiService.py
+++ services/OpenaiService.py
@@ -56,7 +56,7 @@ class OpenaiService():
         '''
         try:
             response = openai.ChatCompletion.create(
-                model="gpt-4-0314",  # 对话模型的名称
+                model="gpt-3.5-turbo",  # 对话模型的名称
                 messages=session,
                 temperature=0.9,  # 值在[0,1]之间，越大表示回复越具有不确定性
                 #max_tokens=4096,  # 回复最大的字符数
