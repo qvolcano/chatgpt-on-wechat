154a5396970488fea022e8658ece646ecd0520f8
qvolcano <qvolcano@126.com>
2023-03-21 16:34:22 +0800

    Update chat_receive.py

--- action/chat_receive.py
+++ action/chat_receive.py
@@ -1,3 +1,10 @@
 async def defaul(args):
-  
-  pass
+  query=args.get("text")
+  context=args
+  chat_ai=PrivoderManager.get("config").get("chat_ai")
+  if chat_ai == "bing":
+    ServiceManager.start("BingService")
+    return ServiceManager.get("BingService").reply(query, context)
+  else:
+    ServiceManager.start("OpenaiService")
+    return await ServiceManager.get("OpenaiService").reply(query, context)
