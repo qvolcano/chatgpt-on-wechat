5d2b8a88528305ad514d6d4e4418d5af6aa826b8
qvolcano <qvolcano@126.com>
2023-03-21 16:27:24 +0800

    Update BingService.py

--- services/BingService.py
+++ services/BingService.py
@@ -16,15 +16,6 @@ class Service:
     def stop(self):
         self.bot.close()
         pass
-    def reply(self, query, context=None):
-        result = ""
-        async def post():
-            resp=await self.bot.ask(prompt=query, conversation_style=ConversationStyle.creative)
-            #return str(resp)
-            message=resp['item']['messages'][1]
-            result = message['text']
-            return message['text']
-        get_future = asyncio.ensure_future(post()) # 相当于开启一个future
-        asyncio.get_event_loop().run_until_complete(get_future) # 事件循环
-        return get_future.result()
+    def async reply(self, query, context=None):
+        return await self.bot.ask_stream(prompt=query, conversation_style=ConversationStyle.creative)
 default = Service
