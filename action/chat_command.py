205f503c129868237b060cee4be3a26c369fdbbc
qvolcano <qvolcano@126.com>
2023-03-21 16:36:25 +0800

    Update chat_command.py

--- action/chat_command.py
+++ action/chat_command.py
@@ -1,2 +1,5 @@
-async def default():
-  pass
+async def default(args):
+  permission=None
+  user=args.get("FromUser")
+  permission=PermissionManager.getUserGroup(user)
+  return await ActionManager.run_async(name,args,userGroup="admin")
